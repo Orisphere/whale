d02aa920459b8c693e1fe3502d2382e021a7cf67
Claire Abu-Assal <spooky121@gmail.com>
2016-04-12 19:37:49 -0700

    More octopus routine.

--- octopus.py
+++ octopus.py
@@ -11,7 +11,7 @@ class Octopus(Enemy):
 		self.speed = [2, 1]
 		self.counter = 0
 		self.health = 100
-		print(self.rect.center)
+	
 	def update(self):
 		self.move()
 		self.counter += 1
@@ -19,35 +19,46 @@ class Octopus(Enemy):
 	def move(self):
 		if self.counter == 50 or self.counter == 150:
 			self.speed[1] = -1 #y-axis speed
-			print(self.rect.center)	
+		
 		elif self.counter == 100 or self.counter == 200:
 			self.speed[1] = 1 
-		
-			print(self.rect.center)	
+	#drop	
 		elif self.counter == 275:
 			self.speed[0] = 0
 			self.speed[1] = 2
-			print(self.rect.center)	
+	#shoot	
 		elif self.counter == 325:
 			self.speed = [0, 0]
 			self.shoot()
-				
-			print(self.rect.center)	
-		elif self.counter == 345:
+	#left			
+		elif self.counter == 355:
 			self.speed[0] = -2
-
-			print(self.rect.center)	
-		elif self.counter == 482:
+	#shoot
+		elif self.counter == 492:
 			self.speed = [0, 0]
 			self.shoot()
-			print(self.rect.center)
-		elif self.counter == 512:
+	#left	
+		elif self.counter == 522:
 			self.speed[0] = -2
-
-		elif self.counter == 650:
+	#shoot
+		elif self.counter == 660:
 			self.speed[0] = 0
-			print(self.rect.center)
+	#drop	
+		elif self.counter == 690:
+			self.speed[1] = 2
+	#bounce		
+		elif self.counter == 785: 
+			self.speed = [2, -1]
+
+		elif self.counter == 835 or self.counter == 935:
+			self.speed[1] = 1 #y-axis speed
 		
+		elif self.counter == 885 or self.counter == 985:
+			self.speed[1] = -1 
+		
+		elif self.counter == 1060:
+			self.speed = [0,0]
+			print(self.rect.center)
 		new_pos = self.rect.move(self.speed)
 		self.rect = new_pos
 
