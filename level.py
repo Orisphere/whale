db807b80dc214eafb5666abf146c31cfd69b1db4
Claire Abu-Assal <spooky121@gmail.com>
2016-10-03 15:56:32 -0700

    Got a preliminary very of Level() working.

--- level.py
+++ level.py
@@ -4,8 +4,11 @@ import random
 class Level(Object):
 
 	def __init__(self):
-		self.rooms = ['StartState', 'Room_1', 'Room_2', 'Room_3', 'Room_4', 'Room_5','Win'] 
+		self.generate_rooms()
 		
-
 	def next_room(self):
 		return self.rooms.pop(0)
+
+	
+	def generate_rooms(self):	
+		self.rooms = ['StartState', 'Room_1', 'Room_2', 'Room_3', 'Room_4', 'Room_5','Win'] 
